75e191e2fdd23ec0542bb0cb47bfaf8d981311e3
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-04-17 15:24:52 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,3 +1,4 @@
+
 // encontrar o botão adicionar tarefa
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
@@ -6,6 +7,10 @@ const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
@@ -27,8 +32,19 @@ function criarElementoTarefa(tarefa) {
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
 
+    botao.onclick = () => {
+        // debugger
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        // console.log('Nova descrição da tarefa: ', novaDescricao)
+        if (novaDescricao) {            
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
+    }
+
     const imagemBotao = document.createElement('img')
-    imagemBotao.setAttribute('src', './imagens/edit.png')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
     botao.append(imagemBotao)
 
     li.append(svg)
@@ -50,7 +66,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
